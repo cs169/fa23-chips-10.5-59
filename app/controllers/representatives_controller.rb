aa67f7a72ad4849e764a4f3c8e00cf6fcb56fe2c
Amar Sehgal <amar.sehgal@berkeley.edu>
2023-11-22 01:57:38 +0000

    Amar Part 1 Attempt 1

--- app/controllers/representatives_controller.rb
+++ app/controllers/representatives_controller.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class RepresentativesController < ApplicationController
+  def show
+    @representative = Representative.find(params[:id])
+  end
+  
   def index
     @representatives = Representative.all
   end
