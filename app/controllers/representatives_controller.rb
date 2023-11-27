4d6c3efb5a7d4271fc9e89c42cc7da5d2307ba36
Desiree Garcia <desiree.garcia@berkeley.edu>
2023-11-27 03:04:13 +0000

    updated controller to show new information. Not entirely sure if this does in the controller or the model as of now. "

--- app/controllers/representatives_controller.rb
+++ app/controllers/representatives_controller.rb
@@ -3,5 +3,14 @@
 class RepresentativesController < ApplicationController
   def index
     @representatives = Representative.all
+
+  # not sure if this belongs here or the model
+    @representatives.each do |rep|
+      #fetch_representative_info is part of civic API, ocd_id = rep identifier
+      civic = GoogleCivicApi.fetch_representative_info(rep.ocd_id)
+      rep.address = civic['address']
+      rep.political_party = civic['party']
+      rep.photo = civic['photo']
+      rep.save
   end
 end
