34cbeb97773825708bbb3d244e3b13777a93dc9c
Desiree Garcia <desiree.garcia@berkeley.edu>
2023-11-28 20:55:12 +0000

    fixed syntax error.

--- app/controllers/representatives_controller.rb
+++ app/controllers/representatives_controller.rb
@@ -4,30 +4,26 @@ require 'google/apis/civicinfo_v2'
 class RepresentativesController < ApplicationController
   def index
     @representatives = Representative.all
-
-    #assign new info to reps, 
-    @representatives.each do |rep|
-
-      #using logic from serach controller to make api requests
-      service = Google::Apis::CivicinfoV2::CivicInfoService.new
-      service.key = Rails.application.credentials[:GOOGLE_API_KEY]
-      rep_info = service.representative_info_by_address(address: params[:address])
-
-      if rep_info&.officials&.any? #make sure its not nil
-        ### get info from representativeInfoByAddress from the officials section
-        curr_rep = rep_info.officials.find { |official| official["name"] == rep.name }
-        if curr_rep  #make sure we find it
-          rep.address = curr_rep['address']
-          rep.political_party = curr_rep['party']
-          rep.photo = curr_rep['photoUrl']
-          rep.save
+  
+    @representatives.each do |rep| # assign new info to reps
+      begin
+        # using logic from search controller to make API requests
+        service = Google::Apis::CivicinfoV2::CivicInfoService.new
+        service.key = Rails.application.credentials[:GOOGLE_API_KEY]
+        rep_info = service.representative_info_by_address(address: params[:address])
+  
+        if rep_info&.officials&.any? # make sure it's not nil
+          # get info from representativeInfoByAddress from the officials section
+          curr_rep = rep_info.officials.find { |official| official["name"] == rep.name }
+          if curr_rep  # make sure we find it
+            rep.address = curr_rep['address']
+            rep.political_party = curr_rep['party']
+            rep.photo = curr_rep['photoUrl']
+            rep.save
+          end
         end
-      end
-    
-      ##handling api errors##
       rescue Google::Apis::CivicinfoV2::Error => api_error
         handle_api_errors(api_error, rep)
-      #other errors
       rescue StandardError => error
         Rails.logger.error("Unexpected error for #{rep.name}: #{error.message}")
       end
@@ -43,6 +39,8 @@ class RepresentativesController < ApplicationController
       Rails.logger.error("Representative not found!")
     else #any other errors
       Rails.logger.error("API error occured! Status code #{status_code}")
+      raise api_error
     end
   end
+
 end
