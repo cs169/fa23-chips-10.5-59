cb8592b5fcb8c67b32f8a579b5d02f018f10247e
Desiree Garcia <desiree.garcia@berkeley.edu>
2023-11-29 03:43:57 +0000

    newest changes

--- app/controllers/representatives_controller.rb
+++ app/controllers/representatives_controller.rb
@@ -4,44 +4,10 @@ require 'google/apis/civicinfo_v2'
 class RepresentativesController < ApplicationController
   def index
     @representatives = Representative.all
-  
-    @representatives.each do |rep| # assign new info to reps
-      begin
-        # using logic from search controller to make API requests
-        service = Google::Apis::CivicinfoV2::CivicInfoService.new
-        service.key = Rails.application.credentials[:GOOGLE_API_KEY]
-        rep_info = service.representative_info_by_address(address: params[:address])
-  
-        if rep_info&.officials&.any? # make sure it's not nil
-          # get info from representativeInfoByAddress from the officials section
-          curr_rep = rep_info.officials.find { |official| official["name"] == rep.name }
-          if curr_rep  # make sure we find it
-            rep.address = curr_rep['address']
-            rep.political_party = curr_rep['party']
-            rep.photo = curr_rep['photoUrl']
-            rep.save
-          end
-        end
-      rescue Google::Apis::CivicinfoV2::Error => api_error
-        handle_api_errors(api_error, rep)
-      rescue StandardError => error
-        Rails.logger.error("Unexpected error for #{rep.name}: #{error.message}")
-      end
-    end
   end
 
-  ##checks for diff api errors
-  def handle_api_errors(api_error, representative)
-    status_code = api_error.status_code
-    case status_code
-    when 403
-      Rails.logger.error("Cannot provide access to following action.")
-    when 404
-      Rails.logger.error("Representative not found!")
-    else #any other errors
-      Rails.logger.error("API error occured! Status code #{status_code}")
-      raise api_error
-    end
+  def show
+    @representative = Representative.find_by(id: params[:id])
   end
 
 end
--- app/models/representative.rb
+++ app/models/representative.rb
@@ -7,20 +7,39 @@ class Representative < ApplicationRecord
     rep_info.officials.each_with_index do |official, index|
       ocdid_temp = ''
       title_temp = ''
+      ###added##
+      address = ''
+      party = ''
+      pic = 'Photo Unavalible'
+      #####
+  
 
       rep_info.offices.each do |office|
         if office.official_indices.include? index
           title_temp = office.name
-          ocdid_temp = office.division_id
+          ocdid_temp = office.division_id 
         end
       end
+
+      if rep_info&.officials&.any? # make sure it's not nil
+          # get info from representativeInfoByAddress from the officials section
+          curr_rep = rep_info.officials.find { |official| official["name"] == rep.name }
+          if curr_rep  # make sure we find it
+            address = curr_rep['address']
+            party = curr_rep['party']
+            pic = curr_rep['photoUrl']
+          end
+        end
 
       existing_rep = Representative.find_by(ocdid: ocdid_temp, title: title_temp)
 
       if existing_rep
         reps.push(existing_rep)
       else
-        rep = Representative.create!({ name: official.name, ocdid: ocdid_temp, title: title_temp})
+        ##added more paramaters###
+        rep = Representative.create!({ name: official.name, ocdid: ocdid_temp, title: title_temp,
+         address: address, political_party: party, photo: pic})
+        rep.save
         reps.push(rep)
       end
     end
